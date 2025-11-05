059775f1fd67357bf5e29cbc26bde470658d2948
Santiago Bonilla <santiagobonilla@Mac-mini-de-Santiago.local>
2025-11-06 00:21:58 +0100

    new structure

--- Experiment Control/basic.py
+++ Experiment Control/basic.py
@@ -6,13 +6,13 @@ print("CPU usage:", psutil.cpu_percent(interval=1), "%")
 # Memoria RAM
 ram = psutil.virtual_memory()
 print("RAM total:", ram.total / (1024 ** 3), "GB")
-print("RAM usada:", ram.percent, "%")
+print("RAM usage:", ram.percent, "%")
 
 # Disco
 disk = psutil.disk_usage('/')
-print("Uso del disco:", disk.percent, "%")
+print("disk usage:", disk.percent, "%")
 
 # Red
 net = psutil.net_io_counters()
-print("Bytes enviados:", net.bytes_sent)
-print("Bytes recibidos:", net.bytes_recv)
+print("sent Bytes:", net.bytes_sent)
+print("received Bytes:", net.bytes_recv)
